4287d8d76442d43433204b54c2c774631da2b4eb
wacdev <i@wac.tax>
2023-07-06 12:39:51 +0800

    ♨

--- wasm/src/vbyte.rs
+++ wasm/src/vbyte.rs
@@ -5,12 +5,12 @@ use crate::Result;
 
 #[wasm_bindgen]
 pub fn vbyteE(vs: &[f64]) -> Vec<u8> {
-  vbyte::compress_list(&vs.into_iter().map(|i| *i as u64).collect::<Vec<_>>())
+  vbyte::compress_list(&vs.iter().map(|i| *i as u64).collect::<Vec<_>>())
 }
 
 #[wasm_bindgen]
 pub fn vbyteD(vs: &[u8]) -> Result<Vec<f64>> {
-  match vbyte::decompress_list(&vs) {
+  match vbyte::decompress_list(vs) {
     Ok(r) => Ok(r.into_iter().map(|i| i as f64).collect()),
     Err(err) => Err(wasm_bindgen::JsError::new(err)),
   }
@@ -23,5 +23,5 @@ pub fn b64VbyteE(vs: &[f64]) -> String {
 
 #[wasm_bindgen]
 pub fn b64VbyteD(vs: &str) -> Result<Vec<f64>> {
-  Ok(vbyteD(&URL_SAFE_NO_PAD.decode(vs)?)?)
+  vbyteD(&URL_SAFE_NO_PAD.decode(vs)?)
 }
