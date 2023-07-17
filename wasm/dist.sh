00655b1f93332e6cee6e8a5c3e17fb2e197379d4
wacdev <i@wac.tax>
2023-07-17 13:09:21 +0800

    0.0.12

--- wasm/dist.sh
+++ wasm/dist.sh
@@ -7,16 +7,25 @@ _DIR=$(
 git add -A
 git pull
 
+cd $_DIR/pkg
+jq '.name="@w5/wasm"' pkg/package.json | sponge pkg/package.json
+version=$(cat package.json | jq -r '.version')
+npm version patch
+
+cd $_DIR
 ./build.sh
+git add -u
+git commit -m "$version"
+npm publish --access=public
+
+cd $_DIR/pkg
+jq '.name="@w5/wasm-vite"' pkg/package.json | sponge pkg/package.json
 sed -i '/function initSync(module)/,$d' pkg/_.js
 sed -i '/__wbg_init\./d' pkg/_.js
 cat ./_patch.js >>pkg/_.js
 ./patch.coffee
 
-cd $_DIR/pkg
-version=$(cat package.json | jq -r '.version')
-npm version patch
 git add -u
-git commit -m $version
+git commit -m "$version vite"
 git push
 npm publish --access=public
