bcf054b05da05ae4d1e9edcd52959dce57e38b24
wacdev <i@wac.tax>
2023-07-24 13:27:00 +0800

    0.0.30 vite

--- wasm/pkg/_.js
+++ wasm/pkg/_.js
@@ -1,7 +1,4 @@
-let imports = {};
-imports['__wbindgen_placeholder__'] = module.exports;
 let wasm;
-const { TextDecoder, TextEncoder } = require(`util`);
 
 const heap = new Array(128).fill(undefined);
 
@@ -32,9 +29,9 @@ function takeObject(idx) {
     return ret;
 }
 
-let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+const cachedTextDecoder = (typeof TextDecoder !== 'undefined' ? new TextDecoder('utf-8', { ignoreBOM: true, fatal: true }) : { decode: () => { throw Error('TextDecoder not available') } } );
 
-cachedTextDecoder.decode();
+if (typeof TextDecoder !== 'undefined') { cachedTextDecoder.decode(); };
 
 let cachedUint8Memory0 = null;
 
@@ -91,7 +88,7 @@ function passArray8ToWasm0(arg, malloc) {
 * @param {Uint8Array} bin
 * @returns {string}
 */
-module.exports.b64E = function(bin) {
+export function b64E(bin) {
     let deferred2_0;
     let deferred2_1;
     try {
@@ -108,9 +105,9 @@ module.exports.b64E = function(bin) {
         wasm.__wbindgen_add_to_stack_pointer(16);
         wasm.__wbindgen_free(deferred2_0, deferred2_1, 1);
     }
-};
+}
 
-let cachedTextEncoder = new TextEncoder('utf-8');
+const cachedTextEncoder = (typeof TextEncoder !== 'undefined' ? new TextEncoder('utf-8') : { encode: () => { throw Error('TextEncoder not available') } } );
 
 const encodeString = (typeof cachedTextEncoder.encodeInto === 'function'
     ? function (arg, view) {
@@ -171,7 +168,7 @@ function getArrayU8FromWasm0(ptr, len) {
 * @param {string} bin
 * @returns {Uint8Array}
 */
-module.exports.b64D = function(bin) {
+export function b64D(bin) {
     try {
         const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
         const ptr0 = passStringToWasm0(bin, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
@@ -190,13 +187,13 @@ module.exports.b64D = function(bin) {
     } finally {
         wasm.__wbindgen_add_to_stack_pointer(16);
     }
-};
+}
 
 /**
 * @param {number} n
 * @returns {Uint8Array}
 */
-module.exports.u64Bin = function(n) {
+export function u64Bin(n) {
     try {
         const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
         wasm.u64Bin(retptr, n);
@@ -208,13 +205,13 @@ module.exports.u64Bin = function(n) {
     } finally {
         wasm.__wbindgen_add_to_stack_pointer(16);
     }
-};
+}
 
 /**
 * @param {number} n
 * @returns {string}
 */
-module.exports.u64B64 = function(n) {
+export function u64B64(n) {
     let deferred1_0;
     let deferred1_1;
     try {
@@ -229,7 +226,7 @@ module.exports.u64B64 = function(n) {
         wasm.__wbindgen_add_to_stack_pointer(16);
         wasm.__wbindgen_free(deferred1_0, deferred1_1, 1);
     }
-};
+}
 
 let cachedFloat64Memory0 = null;
 
@@ -250,7 +247,7 @@ function passArrayF64ToWasm0(arg, malloc) {
 * @param {Float64Array} vs
 * @returns {Uint8Array}
 */
-module.exports.vbyteE = function(vs) {
+function _vbyteE(vs) {
     try {
         const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
         const ptr0 = passArrayF64ToWasm0(vs, wasm.__wbindgen_malloc);
@@ -264,7 +261,7 @@ module.exports.vbyteE = function(vs) {
     } finally {
         wasm.__wbindgen_add_to_stack_pointer(16);
     }
-};
+}
 
 function getArrayF64FromWasm0(ptr, len) {
     ptr = ptr >>> 0;
@@ -274,7 +271,7 @@ function getArrayF64FromWasm0(ptr, len) {
 * @param {Uint8Array} vs
 * @returns {Float64Array}
 */
-module.exports.vbyteD = function(vs) {
+export function vbyteD(vs) {
     try {
         const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
         const ptr0 = passArray8ToWasm0(vs, wasm.__wbindgen_malloc);
@@ -293,13 +290,13 @@ module.exports.vbyteD = function(vs) {
     } finally {
         wasm.__wbindgen_add_to_stack_pointer(16);
     }
-};
+}
 
 /**
 * @param {Float64Array} vs
 * @returns {string}
 */
-module.exports.b64VbyteE = function(vs) {
+export function b64VbyteE(vs) {
     let deferred2_0;
     let deferred2_1;
     try {
@@ -316,13 +313,13 @@ module.exports.b64VbyteE = function(vs) {
         wasm.__wbindgen_add_to_stack_pointer(16);
         wasm.__wbindgen_free(deferred2_0, deferred2_1, 1);
     }
-};
+}
 
 /**
 * @param {string} vs
 * @returns {Float64Array}
 */
-module.exports.b64VbyteD = function(vs) {
+export function b64VbyteD(vs) {
     try {
         const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
         const ptr0 = passStringToWasm0(vs, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
@@ -341,12 +338,12 @@ module.exports.b64VbyteD = function(vs) {
     } finally {
         wasm.__wbindgen_add_to_stack_pointer(16);
     }
-};
+}
 
 const BinMapFinalization = new FinalizationRegistry(ptr => wasm.__wbg_binmap_free(ptr >>> 0));
 /**
 */
-class BinMap {
+export class BinMap {
 
     static __wrap(ptr) {
         ptr = ptr >>> 0;
@@ -410,12 +407,11 @@ class BinMap {
         return takeObject(ret);
     }
 }
-module.exports.BinMap = BinMap;
 
 const BinSetFinalization = new FinalizationRegistry(ptr => wasm.__wbg_binset_free(ptr >>> 0));
 /**
 */
-class BinSet {
+export class BinSet {
 
     static __wrap(ptr) {
         ptr = ptr >>> 0;
@@ -496,56 +492,117 @@ class BinSet {
         return ret >>> 0;
     }
 }
-module.exports.BinSet = BinSet;
 
-module.exports.__wbindgen_memory = function() {
-    const ret = wasm.memory;
-    return addHeapObject(ret);
+async function __wbg_load(module, imports) {
+    if (typeof Response === 'function' && module instanceof Response) {
+        if (typeof WebAssembly.instantiateStreaming === 'function') {
+            try {
+                return await WebAssembly.instantiateStreaming(module, imports);
+
+            } catch (e) {
+                if (module.headers.get('Content-Type') != 'application/wasm') {
+                    console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+
+                } else {
+                    throw e;
+                }
+            }
+        }
+
+        const bytes = await module.arrayBuffer();
+        return await WebAssembly.instantiate(bytes, imports);
+
+    } else {
+        const instance = await WebAssembly.instantiate(module, imports);
+
+        if (instance instanceof WebAssembly.Instance) {
+            return { instance, module };
+
+        } else {
+            return instance;
+        }
+    }
+}
+
+function __wbg_get_imports() {
+    const imports = {};
+    imports.wbg = {};
+    imports.wbg.__wbindgen_memory = function() {
+        const ret = wasm.memory;
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_buffer_085ec1f694018c4f = function(arg0) {
+        const ret = getObject(arg0).buffer;
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_newwithbyteoffsetandlength_6da8e527659b86aa = function(arg0, arg1, arg2) {
+        const ret = new Uint8Array(getObject(arg0), arg1 >>> 0, arg2 >>> 0);
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbindgen_object_drop_ref = function(arg0) {
+        takeObject(arg0);
+    };
+    imports.wbg.__wbg_new_8125e318e6245eed = function(arg0) {
+        const ret = new Uint8Array(getObject(arg0));
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbindgen_jsval_eq = function(arg0, arg1) {
+        const ret = getObject(arg0) === getObject(arg1);
+        return ret;
+    };
+    imports.wbg.__wbindgen_object_clone_ref = function(arg0) {
+        const ret = getObject(arg0);
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbindgen_error_new = function(arg0, arg1) {
+        const ret = new Error(getStringFromWasm0(arg0, arg1));
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbindgen_throw = function(arg0, arg1) {
+        throw new Error(getStringFromWasm0(arg0, arg1));
+    };
+
+    return imports;
+}
+
+function __wbg_init_memory(imports, maybe_memory) {
+
+}
+
+function __wbg_finalize_init(instance, module) {
+    wasm = instance.exports;
+    cachedFloat64Memory0 = null;
+    cachedInt32Memory0 = null;
+    cachedUint32Memory0 = null;
+    cachedUint8Memory0 = null;
+
+
+    return wasm;
+}
+
+import wasm_mod from "./__bg.wasm?url";
+
+
+export const wrapArgsLi = (func)=>(...args)=>{
+  var li = [];
+  for(const i of args){
+    if Array.isArray(i) {
+      li = li.concat(i);
+    }else{
+      li.push(i);
+    }
+  }
+  return func(li)
 };
 
-module.exports.__wbg_buffer_085ec1f694018c4f = function(arg0) {
-    const ret = getObject(arg0).buffer;
-    return addHeapObject(ret);
-};
+export const vbyteE = wrapArgsLi(_vbyteE);
 
-module.exports.__wbg_newwithbyteoffsetandlength_6da8e527659b86aa = function(arg0, arg1, arg2) {
-    const ret = new Uint8Array(getObject(arg0), arg1 >>> 0, arg2 >>> 0);
-    return addHeapObject(ret);
-};
-
-module.exports.__wbindgen_object_drop_ref = function(arg0) {
-    takeObject(arg0);
-};
-
-module.exports.__wbg_new_8125e318e6245eed = function(arg0) {
-    const ret = new Uint8Array(getObject(arg0));
-    return addHeapObject(ret);
-};
-
-module.exports.__wbindgen_jsval_eq = function(arg0, arg1) {
-    const ret = getObject(arg0) === getObject(arg1);
-    return ret;
-};
-
-module.exports.__wbindgen_object_clone_ref = function(arg0) {
-    const ret = getObject(arg0);
-    return addHeapObject(ret);
-};
-
-module.exports.__wbindgen_error_new = function(arg0, arg1) {
-    const ret = new Error(getStringFromWasm0(arg0, arg1));
-    return addHeapObject(ret);
-};
-
-module.exports.__wbindgen_throw = function(arg0, arg1) {
-    throw new Error(getStringFromWasm0(arg0, arg1));
-};
-
-const path = require('path').join(__dirname, '__bg.wasm');
-const bytes = require('fs').readFileSync(path);
-
-const wasmModule = new WebAssembly.Module(bytes);
-const wasmInstance = new WebAssembly.Instance(wasmModule, imports);
-wasm = wasmInstance.exports;
-module.exports.__wasm = wasm;
-
+await (async () => {
+const imports = __wbg_get_imports();
+__wbg_init_memory(imports);
+const { instance, module } = await __wbg_load(
+  await fetch(wasm_mod), imports
+);
+__wbg_finalize_init(instance, module);
+})();
+const newCls = (cls)=>(...args)=>new cls(...args);export const binMap = newCls(BinMap);export const binSet = newCls(BinSet);
