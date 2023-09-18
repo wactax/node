01904f4fc60ccf81967754ad7bca72b86e2dbda5
wacdev <i@wac.tax>
2023-09-18 17:47:44 +0800

    0.0.1

--- binmap/pkg/_.js
+++ binmap/pkg/_.js
@@ -17,13 +17,14 @@ function getUint8Memory0() {
 }
 
 function getStringFromWasm0(ptr, len) {
+    ptr = ptr >>> 0;
     return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
 }
 
 let WASM_VECTOR_LEN = 0;
 
 function passArray8ToWasm0(arg, malloc) {
-    const ptr = malloc(arg.length * 1);
+    const ptr = malloc(arg.length * 1, 1) >>> 0;
     getUint8Memory0().set(arg, ptr / 1);
     WASM_VECTOR_LEN = arg.length;
     return ptr;
@@ -39,24 +40,26 @@ function getInt32Memory0() {
 }
 
 function getArrayU8FromWasm0(ptr, len) {
+    ptr = ptr >>> 0;
     return getUint8Memory0().subarray(ptr / 1, ptr / 1 + len);
 }
 
-const BinMapFinalization = new FinalizationRegistry(ptr => wasm.__wbg_binmap_free(ptr));
+const BinMapFinalization = new FinalizationRegistry(ptr => wasm.__wbg_binmap_free(ptr >>> 0));
 /**
 */
 class BinMap {
 
     static __wrap(ptr) {
+        ptr = ptr >>> 0;
         const obj = Object.create(BinMap.prototype);
-        obj.ptr = ptr;
-        BinMapFinalization.register(obj, obj.ptr, obj);
+        obj.__wbg_ptr = ptr;
+        BinMapFinalization.register(obj, obj.__wbg_ptr, obj);
         return obj;
     }
 
     __destroy_into_raw() {
-        const ptr = this.ptr;
-        this.ptr = 0;
+        const ptr = this.__wbg_ptr;
+        this.__wbg_ptr = 0;
         BinMapFinalization.unregister(this);
         return ptr;
     }
@@ -80,7 +83,7 @@ class BinMap {
         const len0 = WASM_VECTOR_LEN;
         const ptr1 = passArray8ToWasm0(val, wasm.__wbindgen_malloc);
         const len1 = WASM_VECTOR_LEN;
-        wasm.binmap_set(this.ptr, ptr0, len0, ptr1, len1);
+        wasm.binmap_set(this.__wbg_ptr, ptr0, len0, ptr1, len1);
     }
     /**
     * @param {Uint8Array} key
@@ -91,15 +94,15 @@ class BinMap {
             const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
             const ptr0 = passArray8ToWasm0(key, wasm.__wbindgen_malloc);
             const len0 = WASM_VECTOR_LEN;
-            wasm.binmap_get(retptr, this.ptr, ptr0, len0);
+            wasm.binmap_get(retptr, this.__wbg_ptr, ptr0, len0);
             var r0 = getInt32Memory0()[retptr / 4 + 0];
             var r1 = getInt32Memory0()[retptr / 4 + 1];
-            let v1;
+            let v2;
             if (r0 !== 0) {
-                v1 = getArrayU8FromWasm0(r0, r1).slice();
+                v2 = getArrayU8FromWasm0(r0, r1).slice();
                 wasm.__wbindgen_free(r0, r1 * 1);
             }
-            return v1;
+            return v2;
         } finally {
             wasm.__wbindgen_add_to_stack_pointer(16);
         }
@@ -110,12 +113,12 @@ class BinMap {
     dump() {
         try {
             const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-            wasm.binmap_dump(retptr, this.ptr);
+            wasm.binmap_dump(retptr, this.__wbg_ptr);
             var r0 = getInt32Memory0()[retptr / 4 + 0];
             var r1 = getInt32Memory0()[retptr / 4 + 1];
-            var v0 = getArrayU8FromWasm0(r0, r1).slice();
+            var v1 = getArrayU8FromWasm0(r0, r1).slice();
             wasm.__wbindgen_free(r0, r1 * 1);
-            return v0;
+            return v1;
         } finally {
             wasm.__wbindgen_add_to_stack_pointer(16);
         }
@@ -124,7 +127,7 @@ class BinMap {
     * @returns {number}
     */
     len() {
-        const ret = wasm.binmap_len(this.ptr);
+        const ret = wasm.binmap_len(this.__wbg_ptr);
         return ret >>> 0;
     }
     /**
--- binmap/pkg/__bg.wasm.d.ts
+++ binmap/pkg/__bg.wasm.d.ts
@@ -8,6 +8,6 @@ export function binmap_get(a: number, b: number, c: number, d: number): void;
 export function binmap_dump(a: number, b: number): void;
 export function binmap_len(a: number): number;
 export function binmap_load(a: number, b: number): number;
-export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
 export function __wbindgen_add_to_stack_pointer(a: number): number;
-export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
